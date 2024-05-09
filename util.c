be795577cc8897fa0cfb0e1e4e54711ad7c801a0
hungeredJims <sp0943@cau.ac.kr>
2024-05-09 18:25:23 +0900

    util error mod

--- util.c
+++ util.c
@@ -6,38 +6,43 @@
 /*   By: jihyjeon <jihyjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 13:00:01 by jihyjeon          #+#    #+#             */
-/*   Updated: 2024/05/09 13:38:53 by jihyjeon         ###   ########.fr       */
+/*   Updated: 2024/05/09 18:24:21 by jihyjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	*path_finder(char *path, char *cmd);
+char	*path_finder(char **path, char *cmd);
 
 char	*get_path(char *cmd, char **envp)
 {
 	char	*final;
 	int		idx;
+	char	**path_arr;
 
 	idx = 0;
 	while (ft_strnstr(envp[idx], "PATH", 4) == 0)
 		idx++;
-	final = path_finder(envp[idx], cmd);
+	path_arr = ft_split(envp[idx] + 5, ':');
+	final = path_finder(path_arr, cmd);
+	idx = 0;
+	while (path_arr[idx++])
+		free(path_arr[idx]);
+	free(path_arr);
 	return (final);
 }
 
-char	*path_finder(char *path, char *cmd)
+char	*path_finder(char **path, char *cmd)
 {
-	char	**path_arr;
+	
 	char	*extract;
 	char	*ans;
 	int		idx;
 
-	path_arr = ft_split(path + 5, ':');
 	idx = 0;
-	while (path_arr[idx])
+	while (path[idx])
 	{
-		extract = ft_strjoin(path_arr[idx], "/");
+		extract = ft_strjoin(path[idx], "/");
 		ans = ft_strjoin(extract, cmd);
 		free(extract);
 		if (access(ans, X_OK) == 0)
@@ -45,10 +50,6 @@ char	*path_finder(char *path, char *cmd)
 		free(ans);
 		idx++;
 	}
-	idx = 0;
-	while (path_arr[idx++])
-		free(path_arr[idx]);
-	free(path_arr);
 	return (ans);
 }
 
